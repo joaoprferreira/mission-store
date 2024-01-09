e7142445528afa7ed34aa91f9dece19b6cc1d352
João Pedro Ferreira <joaof082@gmail.com>
2024-01-08 22:48:48 -0300

    feat: starting building screen of register new product
    Signed-off-by: João Pedro Ferreira <joaof082@gmail.com>

---
+++ src/app/registerNewProduct/page.tsx
@@ -0,0 +1,66 @@
+'use client'
+import * as React from 'react'
+
+import { useForm } from 'react-hook-form'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { registerFormData, registerNewProductSchema } from './types'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+export default function RegisterNewProduct() {
+  const { register } = useForm<registerFormData>({
+    resolver: zodResolver(registerNewProductSchema),
+  })
+
+  return (
+    <div className="flex items-center justify-center w-[100%] h-screen bg-red">
+      <Card>
+        <CardHeader className="flex items-center justify-center w-[100%]">
+          <CardTitle>Cadastre um novo produto</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Nome do produto</Label>
+                <Input
+                  id="name"
+                  placeholder="Digite o nome do produto..."
+                  {...register('nameProduct')}
+                />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="framework">Preço</Label>
+                <Input
+                  prefix="R$:"
+                  placeholder="Digite o preço do produto..."
+                  {...register('priceProduct')}
+                />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="framework">Descrição</Label>
+                <Input
+                  placeholder="Digite uma descrição para o produto..."
+                  {...register('descriptionProduct')}
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancelar</Button>
+          <Button>Cadastrar</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
