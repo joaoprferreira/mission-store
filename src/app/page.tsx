c6b47dc0fb63b28eb0b8260c97cc6ab4c7aaea2a
João Pedro Ferreira <joaof082@gmail.com>
2024-01-08 22:47:19 -0300

    feat: starting page main and testing card of products
    Signed-off-by: João Pedro Ferreira <joaof082@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import { Cart } from '@/components/cart'
+import Header from '@/app/components/header'
+import Products from '@/components/products'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex  max-h-screen flex-col p-20 gap-20">
+      <Header />
+      <div className="grid grid-cols-3 gap-4 overflow-auto h-screen items-center">
+        <Products />
+        <Products />
+        <Products />
+        <Products />
+        <Products />
+        <Products />
+        <Products />
+        <Products />
+        <Products />
+        <Products />
+        <Products />
+        <Products />
+      </div>
+    </main>
   )
 }
