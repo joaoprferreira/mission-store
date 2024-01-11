6556ccd35ab563012e2025f7c192d2d8b39d3a06
João Pedro Ferreira <joaof082@gmail.com>
2024-01-10 22:34:16 -0300

    refactor: removing prisma of application
    Signed-off-by: João Pedro Ferreira <joaof082@gmail.com>

--- prisma/migrations/20240109071438_creating_table_product/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "products" (
-    "productId" TEXT NOT NULL PRIMARY KEY,
-    "nameProduct" TEXT NOT NULL,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
