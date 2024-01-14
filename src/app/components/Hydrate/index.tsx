5c4680061d74dbcffebfb1c611f5fcb659a30e95
João Pedro Ferreira <joaof082@gmail.com>
2024-01-14 15:16:00 -0300

    feat: component to hydrate re-renders
    Signed-off-by: João Pedro Ferreira <joaof082@gmail.com>

--- src/app/components/Hydrate/index.tsx
+++ src/app/components/Hydrate/index.tsx
@@ -1,5 +1,7 @@
 'use client'
+import MissionLogo from '@/assets/logo-mission.png'
 
+import Image from 'next/image'
 import { ReactNode, useEffect, useState } from 'react'
 
 export default function Hydrate({ children }: { children: ReactNode }) {
@@ -9,5 +11,12 @@ export default function Hydrate({ children }: { children: ReactNode }) {
     setIsMounted(true)
   }, [])
 
-  return isMounted ? <>{children}</> : <span>Carregando...</span>
+  return isMounted ? (
+    <>{children}</>
+  ) : (
+    <div className="flex flex-col gap-5 items-center justify-center h-screen animate-pulse">
+      <Image src={MissionLogo} alt="logo da mission" width={100} />
+      Carregando...
+    </div>
+  )
 }
