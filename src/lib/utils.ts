a3b2b5fbc40f7e33813f99459932cc1c47670659
João Pedro Ferreira <joaof082@gmail.com>
2024-01-14 15:03:23 -0300

    feat: building utils to format price
    Signed-off-by: João Pedro Ferreira <joaof082@gmail.com>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,10 +6,18 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const formatPrice = (price: number | null) => {
-  if (!price) return 'R$ 0,00'
+  if (price === null || isNaN(price)) {
+    return 'R$ 0,00'
+  }
 
   return new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL',
   }).format(price / 100)
+}
+
+export const formatPriceInput = (value: any) => {
+  const rawValue = value.replace(/[^\d]/g, '')
+  const formattedValue = (rawValue / 100).toFixed(2)
+  return formattedValue
 }
