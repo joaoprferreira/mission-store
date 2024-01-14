aaf16d537fa74443042b0d0a5560279d1ecad95e
João Pedro Ferreira <joaof082@gmail.com>
2024-01-14 15:16:58 -0300

    feat: adding components in cardProduct
    Signed-off-by: João Pedro Ferreira <joaof082@gmail.com>

--- src/components/cardProduct/index.tsx
+++ src/components/cardProduct/index.tsx
@@ -20,6 +20,7 @@ type TProduct = {
   priceProduct: number
   isCart?: boolean
   product: IProduct
+  quantity: number
 }
 
 export default function CardProduct({
@@ -28,33 +29,42 @@ export default function CardProduct({
   priceProduct,
   isCart,
   product,
+  quantity,
 }: TProduct) {
   const { addToCart, removeFromCart } = useCartStore()
+
+  console.log('Quantity:', quantity)
 
   return (
     <>
       <Card
         className={
           isCart
-            ? `border-none shadow-none rounded-none gap-0 h-[250px]`
+            ? `border-none rounded-none gap-0 h-[250px]`
             : `w-[300px] h-[300px] flex items-center justify-center`
         }
       >
         <CardContent className="flex align-center gap-2 ">
           <CardTitle className="flex gap-2">
             {isCart ? (
-              <CardDescription className="text-xl">Nome:</CardDescription>
+              <CardDescription className="text-xl">
+                Nome: {nameProduct}
+              </CardDescription>
             ) : (
-              <CardTitle>{nameProduct}</CardTitle>
+              <CardTitle className="flex text-center">
+                {nameProduct.toLowerCase()}
+              </CardTitle>
             )}
           </CardTitle>
         </CardContent>
         <CardContent className="flex align-center gap-2 ">
           <CardTitle className="flex gap-2">
             {isCart ? (
-              <CardDescription className="text-xl">Descrição:</CardDescription>
+              <CardDescription className="text-xl">
+                Descrição: {descriptionProduct}
+              </CardDescription>
             ) : (
-              <CardDescription className="text-sm color-black">
+              <CardDescription className="text-sm color-black text-center">
                 {descriptionProduct}
               </CardDescription>
             )}
@@ -75,13 +85,13 @@ export default function CardProduct({
         </CardContent>
         {isCart ? (
           <div className="flex items-center gap-5 mx-5">
-            <Button onClick={() => removeFromCart(product)}>
+            <Button onClick={() => removeFromCart(product, false)}>
               <Trash />
             </Button>
 
-            <Button>-</Button>
-            <CardDescription>1</CardDescription>
-            <Button>+</Button>
+            <Button onClick={() => removeFromCart(product, true)}>-</Button>
+            <CardDescription>{quantity}</CardDescription>
+            <Button onClick={() => addToCart(product)}>+</Button>
           </div>
         ) : (
           <CardFooter>
