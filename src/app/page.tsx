787a04a03179d8e46d078cd352180850f3c374e3
João Pedro Ferreira <joaof082@gmail.com>
2024-01-14 15:04:59 -0300

    feat: adding component of listProducts
    Signed-off-by: João Pedro Ferreira <joaof082@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,22 @@
 'use client'
-import Header from '@/app/components/header'
+import { useRouter } from 'next/navigation'
+import Header from '@/app/(products)/_components/header'
 import Products from '@/components/products'
 import useProductStore from '@/store/productStore/useProductStore'
 import { useSession } from 'next-auth/react'
 import { redirect } from 'next/navigation'
+import ListProducts from './(products)/_components/listProducts'
 
 export default function Home() {
-  const getProducts = useProductStore((state) => state.products)
+  const router = useRouter()
   const { data: session } = useSession({
     required: true,
   })
 
   return (
-    <main className="flex  max-h-screen flex-col py-5 pl-5 gap-20">
+    <main className="flex  max-h-screen max-w-screen flex-col py-5 pl-5 gap-20 ">
       <Header />
-      <div className="grid grid-cols-3 gap-4 overflow-auto h-screen items-center">
-        {getProducts.map((item) => (
-          <>
-            <Products product={item} />
-          </>
-        ))}
-      </div>
+      <ListProducts />
     </main>
   )
 }
